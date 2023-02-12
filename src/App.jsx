fb785407255aaa1b786ebac8776663fe2dc3d1db
Adam <adam.aroslanow@gmail.com>
2023-02-12 17:11:48 +0100

    fetching popular shows added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,39 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { TVShowAPI } from "./api/tv-show"
+import { BACKDROP_BASE_URL } from "./config";
+import style from "./style.module.css"
+
 export function App(){
-    return <div>Hello World</div>
+    const [currentTVShow,setCurrentTVShow] = useState();
+
+    async function fetchPopulars() {
+        const popularTShowList = await TVShowAPI.fetchPopulars();
+        if(popularTShowList.length>0){
+            setCurrentTVShow(popularTShowList[0]);
+        }
+    }
+    useEffect(() => {
+        fetchPopulars();   
+    }, []);
+    console.log(currentTVShow);
+
+
+    return <div className={style.main_container} 
+            style={{
+                background: currentTVShow ? `linear-gradient(rgba(0,0,0,0.55),rgba(0,0,0,0.55)),url("${BACKDROP_BASE_URL}${currentTVShow.backdrop_path}") no-repeat center / cover` : "black" }}>
+            <div className={style.header}>
+                <div className="row">
+                    <div className="col-4">
+                        <div>LOGO</div>
+                        <div>SUBTITLE</div>
+                    </div>
+                    <div className ="col-md-12 col-lg-4">
+                        <input style={{width:"100%"}} type="text" />
+                    </div>
+                </div>
+            </div>
+            <div className={style.tv_show_detail}></div>
+            <div className={style.recommended_tv_shows}></div>
+         </div>
 }
---
+++ src/config.js
@@ -0,0 +1,5 @@
+const BASE_URL = "https://api.themoviedb.org/3/";
+const API_KEY_PARAM = "?api_key=8910ecfb75412a863eec7a7cefd512c0";
+const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original"
+
+export { BASE_URL, API_KEY_PARAM, BACKDROP_BASE_URL}
