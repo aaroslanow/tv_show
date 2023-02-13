da2e98c692ceb31b5536e47776d7e2a1203cdbb2
Adam <adam.aroslanow@gmail.com>
2023-02-13 14:28:41 +0100

    initial commit

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const BASE_URL = "https://api.themoviedb.org/3/";
-const API_KEY_PARAM = "?api_key=8910ecfb75412a863eec7a7cefd512c0";
+const API_KEY_PARAM =  process.env.REACT_APP_API_KEY
 const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original"
 const SMALL_IMG = "https://image.tmdb.org/t/p/w300"
 export { BASE_URL, API_KEY_PARAM, BACKDROP_BASE_URL, SMALL_IMG}
